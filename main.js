a92326d6c2f8fe8cbd1544ad55f10b2d62bb4abb
lamekammi <kameronnb@gmail.com>
2022-05-29 13:54:54 -0700

    Main HTML and CSS setup some Functions

---
+++ main.js
@@ -0,0 +1,34 @@
+/*----- constants -----*/
+
+
+/*----- app's state (variables) -----*/
+
+
+/*----- cached element references -----*/
+const tttBoard = document.getElementById('ttt-board');
+const square1 = document.getElementById('square1');
+const square2 = document.getElementById('square2');
+const square3 = document.getElementById('square3');
+const square4 = document.getElementById('square4');
+const square5 = document.getElementById('square5');
+const square6 = document.getElementById('square6');
+const square7 = document.getElementById('square7');
+const square8 = document.getElementById('square8');
+const square9 = document.getElementById('square9');
+const resetButton = document.getElementById('reset-game');
+
+
+/*----- event listeners -----*/
+resetButton.addEventListener('click'. handleClick);
+
+
+/*----- functions -----*/
+function initGame() {
+
+};
+
+function handClick() {
+
+}
+
+
