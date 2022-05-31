c03a5cc3b8fcfda00c2574cf5ca602224463a2f3
lamekammi <kameronnb@gmail.com>
2022-05-30 18:16:27 -0700

    working out JS

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
 /*----- constants -----*/
-
+const winningCombos = {
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+}
 
 /*----- app's state (variables) -----*/
-
+//let gameActive = true;
+let currPlayer = 'Z';
 
 /*----- cached element references -----*/
+const square1 = document.getElementById('square1');
+const square2 = document.getElementById('square2');
+const square3 = document.getElementById('square3');
+const square4 = document.getElementById('square4');
+const square5 = document.getElementById('square5');
+const square6 = document.getElementById('square6');
+const square7 = document.getElementById('square7');
+const square8 = document.getElementById('square8');
+const square9 = document.getElementById('square9');
 const resetButton = document.getElementById('reset-game');
 
 
 /*----- event listeners -----*/
 //resetButton.addEventListener('click'. handleClick);
+//square1.addEventListener('click', handleClick);
 
 
 /*----- functions -----*/
@@ -20,5 +40,9 @@ const resetButton = document.getElementById('reset-game');
 //function handClick() {
 
 //}
+
+//function handleRestartGame() {
+
+//};
 
 
